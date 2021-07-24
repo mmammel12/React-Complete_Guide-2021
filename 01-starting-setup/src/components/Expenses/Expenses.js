063940fab2a8aef948ca466d09800d675d52e59b
Marty Mammel <mmammel12@gmail.com>
2021-07-24 13:16:38 -0500

    section 4 complete

--- 01-starting-setup/src/components/Expenses/Expenses.js
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -1,13 +1,32 @@
+import { useState } from "react";
+
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
-  const expenseItems = props.expenses.map((item) => (
+  const [expenses, setExpenses] = useState(props.expenses);
+  const [selectedYear, setSelectedYear] = useState("2021");
+
+  const expenseItems = expenses.map((item) => (
     <ExpenseItem date={item.date} title={item.title} amount={item.amount} />
   ));
 
-  return <Card className="expenses">{expenseItems}</Card>;
+  const selectedYearChangeHandler = (selectedYear) => {
+    setSelectedYear(selectedYear);
+    console.log(selectedYear);
+  };
+
+  return (
+    <Card className="expenses">
+      <ExpensesFilter
+        selectedYear={selectedYear}
+        onSelectedYearChange={selectedYearChangeHandler}
+      />
+      {expenseItems}
+    </Card>
+  );
 };
 
 export default Expenses;
